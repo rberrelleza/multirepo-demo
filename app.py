f54d687d8170d58d75f11f172c6ff1413eba3860
Pablo Chico de Guzman <pchico83@gmail.com>
2022-03-24 15:53:51 +0100

    Add logic to store votes in MongoDB

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-import os
-from flask import Flask
+from pymongo import MongoClient
+
+
+def lambda_handler(event, context):
+    client = MongoClient('mongodb', username="okteto", password="okteto", authSource="okteto", port=27017)
+    db = client['okteto']
+    coll = db['votes']
+    coll.insert_one(event)
+    return ""
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    msg = 'Hello World!'
-    return msg
+@app.route('/', methods = ['POST'])
+def vote():
+    request_data = request.get_json()
+
+
 
 if __name__ == '__main__':
   print('Starting lambda server...')
-  # comment out to use Pycharm's remote debugger
-  # attach()
-
   app.run(host='0.0.0.0', port=8080)
