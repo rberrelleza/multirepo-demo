d8fbc04e75be59deab8077ab8f1168b09eec2428
Ramiro Berrelleza <ramiro@okteto.com>
2022-03-24 19:06:12 -0700

    read from host

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     username = os.getenv('MONGODB_USERNAME')
     password = os.getenv('MONGODB_PASSWORD')
     database = os.getenv('MONGODB_DATABASE')
-    host = os.getenv('MONGODB_DATABASE', 'mongodb-serverless.tussl.mongodb.net')
+    host = os.getenv('MONGODB_HOST', 'mongodb-serverless.tussl.mongodb.net')
     client = MongoClient('mongodb+srv://' + username + ':' + password + '@'+ host + '/' + database + '?retryWrites=true&w=majority')
     db = client[database]
     coll = db['votes']
